93baf6d9595d82a5ea58c9efd6c195fe057f9c9f
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-01-20 22:07:40 +0700

    Create Camp model and its migration table

---
+++ database/migrations/2022_01_20_145929_create_camps_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('camps', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('slug', 100);
+            $table->unsignedInteger('price');
+            $table->timestamps();
+            $table->softDeletes('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('camps');
+    }
+}
